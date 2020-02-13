1b1631f3eb0523758cd1c56c2577af9004df3e8c
Moiz Ahmed <moizz.ahmed@mail.utoronto.ca>
2020-02-13 11:27:22 -0500

    finish simple color picker extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   title = 'simple-chrome-extension';
   color: string;
   constructor() { }
+  public colorize() {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        { code: 'document.body.style.backgroundColor = "' + this.color + '";' }
+      );
+    });
+  }
 }
